77c308ddc8146ea480b5952b1e4ec9e50a8ac949
Dominik <rusac.dominik@gmail.com>
2020-12-28 20:50:30 +0100

    creating and styling the page

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const word = document.getElementById('word');
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+
+// Rules and close event handlers
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
+
