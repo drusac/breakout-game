531d61ace4c32e6f640932f202fe972423ac52ea
Dominik <rusac.dominik@gmail.com>
2020-12-29 00:52:54 +0100

    lose and reset game

--- script.js
+++ script.js
@@ -133,16 +133,40 @@ function moveBall() {
             if (brick.visible) {
                 if (
                     ball.x - ball.size > brick.x && //left brick side check
-                    ball.x + ball.size < brick.x + rick.w && // right brick side check
+                    ball.x + ball.size < brick.x + brick.w && // right brick side check
                     ball.y + ball.size > brick.y && // top brick side check
                     ball.y - ball.size < brick.y + brick.h // bottom brick side check
                 ) {
                     ball.dy *= -1;
                     brick.visible = false;
+
+                    increaseScore();
                 }
             }
-        })
-    })
+        });
+    });
+
+    // Hit bottom wall - Lose
+    if (ball.y + ball.size > canvas.height) {
+        showAllBricks();
+        score = 0;
+    }
+}
+
+// Increase score
+function increaseScore() {
+    score++;
+
+    if(score % (brickRowCount * brickRowCount) === 0) {
+        showAllBricks();
+    }
+}
+
+// Make all bricks appear
+function showAllBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => (brick.visible = true));
+    });
 }
 
 // Draw everything
